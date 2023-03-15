2568e1feaed1a2dc96111353bf195d7001061872
Yehor Pustovoit <63584ee@gmail.com>
2023-03-15 15:44:41 +0200

    fix: Refactoring

--- lib/app/feature/candidates_list/presentation/candidates_page_cubit/candidates_page_cubit.dart
+++ lib/app/feature/candidates_list/presentation/candidates_page_cubit/candidates_page_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:bloc/bloc.dart';
 
-import 'candidates_page_state.dart';
+import 'package:employee_app/app/feature/candidates_list/presentation/candidates_page_cubit/candidates_page_state.dart';
 
 class CandidatesPageCubit extends Cubit<CandidatesPageState> {
   CandidatesPageCubit() : super(CandidatesPageInitial());
