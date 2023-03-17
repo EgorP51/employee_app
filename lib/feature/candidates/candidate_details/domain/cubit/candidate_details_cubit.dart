7dcf1bd2f9ef3f6eecbd8d80355bcff9387a4590
Yehor Pustovoit <63584ee@gmail.com>
2023-03-17 17:00:00 +0200

    Add basic navigation with passing the model to another screen

--- lib/feature/candidates/candidate_details/domain/cubit/candidate_details_cubit.dart
+++ lib/feature/candidates/candidate_details/domain/cubit/candidate_details_cubit.dart
@@ -2,8 +2,6 @@ import 'package:bloc/bloc.dart';
 
 import 'package:employee_app/feature/candidates/candidate_details/domain/cubit/candidate_details_state.dart';
 
-
-
 class CandidateDetailsCubit extends Cubit<CandidateDetailsState> {
   CandidateDetailsCubit() : super(CandidateDetailsInitial());
 
--- lib/feature/candidates/candidates_list/domain/cubit/candidates_page_cubit.dart
+++ lib/feature/candidates/candidates_list/domain/cubit/candidates_page_cubit.dart
@@ -2,7 +2,6 @@ import 'package:bloc/bloc.dart';
 
 import 'package:employee_app/feature/candidates/candidates_list/domain/cubit/candidates_page_state.dart';
 
-
 class CandidatesPageCubit extends Cubit<CandidatesPageState> {
   CandidatesPageCubit() : super(CandidatesPageInitial());
 
